78c6ab08a0d10cced79cfba9520a4337ab76af68
Rachel Dorn <rachel.m.dorn@gmail.com>
2017-01-05 22:45:22 -0500

    create single proposition page template

---
+++ single-preposition.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post(); ?>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12">
+						<h4><?php the_title()?></h4>
+						<?php $cases = get_field('cases_governed');
+							foreach ($cases as $case) {
+								echo '<p>' . get_field(strtolower($case)) . ' (+' . $case . ')</p>';
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; ?>
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
